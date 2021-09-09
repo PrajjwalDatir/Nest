8c41aded8680c4f17c867fa6b6e166f8533d27a0
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-09-09 21:17:13 +0530

    `POST` products
    added product module. We can add product in the products array

---
+++ src/products/product.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common'
+import { ProductService } from './product.service'
+
+@Controller('products')
+export class ProductsController {
+    constructor(private readonly productService: ProductService) {}
+    @Post()
+    addProduct(@Body('title') title: string, @Body('description') description: string, @Body('price') price: number) {
+        const id = this.productService.insertProduct(title, description, price)
+        return { id }
+    }
+}
---
+++ src/products/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common'
+import { Product } from './product.model'
+import { v4 } from 'uuid'
+
+@Injectable()
+export class ProductService {
+    products: Product[] = []
+    id: string = v4()
+    public insertProduct(title: string, description: string, price: number) {
+        const newProduct = new Product(this.id, title, description, price)
+        this.products.push(newProduct)
+        return this.id
+    }
+}
