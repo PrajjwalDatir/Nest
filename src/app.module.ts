930def7e92df1b7ca51a26e1a606a3ffc562295f
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-09-09 21:16:13 +0530

    added link to product + boilerplate code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ProductsModule } from './products/product.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ProductsModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
